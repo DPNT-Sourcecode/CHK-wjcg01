16906fc6381fb7991e29b4d0b513b5981c0ff553
sbx_user1051 <sbx_user1051@169.254.65.181>
2022-03-30 12:33:30 +0000

    Wed Mar 30 12:33:30 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,10 +7,24 @@ import java.util.HashMap;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
+        
         int sum = 0;
+        int contorA = 0;
+        int contorB = 0;
         for (int i = 0; i < skus.length();i++) {
-            if(skus.charAt(i)="A") {
-
+            if(skus.charAt(i)=='A') {
+                sum += 50;
+                contorA++;
+            }
+            if(skus.charAt(i)=='B') {
+                sum += 30;
+                contorB++;
+            }
+            if(skus.charAt(i)=='C') {
+                sum += 20;
+            }
+            if(skus.charAt(i)=='D') {
+                sum += 15;
             }
         }
 
@@ -19,5 +33,6 @@ public class CheckoutSolution {
         ;
     }
 }
+
 
 
