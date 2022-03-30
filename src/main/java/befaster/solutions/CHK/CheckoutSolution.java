a3fdcec2b217e9ea6de22e5dc03e03503bb78c6b
sbx_user1051 <sbx_user1051@169.254.65.181>
2022-03-30 12:53:52 +0000

    Wed Mar 30 12:53:52 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -12,8 +12,10 @@ public class CheckoutSolution {
         pricesbyProduct.put('B',30);
         pricesbyProduct.put('C',20);
         pricesbyProduct.put('D',15);
+        pricesbyProduct.put('E',40);
         int sum = 0;
         int contorA = 0, contorB = 0;
+        int contorE = 0;
 
 
         for (int i = 0; i < skus.length();i++) {
@@ -21,19 +23,28 @@ public class CheckoutSolution {
                 return -1;
             }
             sum += pricesbyProduct.get(skus.charAt(i));
-            if(skus.charAt(i)=='A') {
+            if(skus.charAt(i) =='A') {
                 contorA++;
             }
-            if(skus.charAt(i)=='B') {
+            if(skus.charAt(i) =='B') {
                 contorB++;
+            }
+            if(skus.charAt(i) =='E') {
+                contorE++;
             }
 
         }
-        sum -= contorA/3 * 20;
+
+        sum -= contorA/5 * 50;
+        sum -= contorA%5/3 > 0 ? 20 : 0;
+        sum -= contorE/2 * 30;
+        contorB -= contorE/2;
         sum -= contorB/2 * 15;
+
 
 
 
         return sum;
     }
 }
+
